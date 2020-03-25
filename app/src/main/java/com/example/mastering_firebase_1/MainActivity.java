ce4174ddece66aaacfbfd0031919460799e726ee
DeAndre Boston <dboston3@yahoo.com>
2020-03-24 22:01:33 -0500

    Write db code

--- app/src/main/java/com/example/mastering_firebase_1/MainActivity.java
+++ app/src/main/java/com/example/mastering_firebase_1/MainActivity.java
@@ -10,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize cloud firestore:
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create new map object to send data to the dv
+        Map<String, Object> user = new HashMap<>();
+
+        //Add data to this map
+        user.put("first", "John");
+        user.put("last", "Cena");
+        user.put("born", "New York");
+
+        //Step 4: Add a new document with a generated ID:
+        db.collection(collectionPath: "users")
+            .add(user).addSuccessListener(new onNew)
+
+
+
     }
 }
